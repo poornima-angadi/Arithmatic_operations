cb4df83559db7bccc204afdc0320be796d5d5741
poornima <01fe24bca200@kletech.ac.in>
2025-10-27 13:57:23 +0530

    Addition and subtraction

---
+++ Add_Sub.py
@@ -0,0 +1,7 @@
+num1=float(input("Enter the first number:-"))
+num2=float(input("Enter the second number:-"))
+addition=num1+num2
+substraction=num1-num2
+print("\n Results")
+print(f"Addition of {num1}+{num2} is {addition}")
+print(f"Substraction of {num1}+{num2} is {substraction}")
