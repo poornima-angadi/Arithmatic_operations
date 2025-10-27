faadc56fa7bce02785fc6b3109bfe1f34df35cd9
poornima <01fe24bca200@kletech.ac.in>
2025-10-27 14:20:45 +0530

    math_operation

---
+++ math_operations.py
@@ -0,0 +1,11 @@
+num1=float(input("Enter the first number:-"))
+num2=float(input("Enter the second number:-"))
+addition=num1+num2
+substraction=num1-num2
+multiply = num1*num2
+division = num1/num2
+print("\n Results")
+print(f"Addition of {num1}+{num2} is {addition}")
+print(f"Substraction of {num1}+{num2} is {substraction}")
+print(f"multiplication of  {num1}+{num2} is {multiply}")
+print(f"division of {num1}+{num2} is {division}")
